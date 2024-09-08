84a3da05f5a8e11122c2a304b36239f97fbbbc28
RudyRM <rudy.richter@alumnos.uach.cl>
2024-09-08 18:32:09 -0300

    procesar cpp archivo

---
+++ procesar.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm> 
+#include <vector>
+#include <cctype> 
+#include <getopt.h>
+#include <sstream>
+#include <fstream>
+#include <cstdlib>
+#include "funciones.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+
+}
