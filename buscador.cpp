df9d68c97d676a1060c0820f18c89bf134592af9
RudyRM <rudy.richter@alumnos.uach.cl>
2024-11-06 23:52:12 -0300

    SOCKETS CREADOS

--- buscador.cpp
+++ buscador.cpp
@@ -3,9 +3,60 @@
 #include <vector>
 #include <map>
 #include <tuple>
+#include <fstream>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 using namespace std;
 
+#define PORT 2020
+
 int main (){
+    ifstream archivo("/home/rudy/2024/SO/SistemasOperativos/complementos/mapa.txt");
+    if (!archivo.is_open()){
+        cerr << "El archivo no se pudo abrir" << endl;
+        return EXIT_FAILURE;
+    }
+
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    string mensaje;
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+        cerr << "Hubo un error al crear el socket" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0){
+        cerr << "Direccion mala" << endl;
+        close(sock);
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+        cerr << "Fallo la conexion" << endl;
+        close(sock);
+        return -1;
+    }
+
+    cout << "Escriba el mensaje, si quiere terminar el programa, escriba 'SALIR AHORA'" << endl;
+    cin >> mensaje;
+
+    send(sock, mensaje.c_str(), mensaje.size(), 0);
+
+    char buffer[1024] = {0};
+    int valread = read(sock, buffer, 1024);
+    cout << "Respuesta desde el servidor: " << buffer << endl;
+
+    close(sock);
+
+
+
+
+
     string palabra;
     cout << "Ingrese una palabra a buscar: ";
     cin >> palabra;
@@ -17,9 +68,13 @@ int main (){
     //Verificamos si es que la palabra buscada está en el caché.
     auto it = cache.find(palabra);
     if (it != cache.end()) {
-        cout << "Palabra: "<< it->first << " encontrada en el caché, con ocurrencia en: " << it->second << endl;
+        //cout << "Palabra: "<< it->first << " encontrada en el caché, con ocurrencia en: " << it->second << endl;
+        cout << "Encontrado en el cache" << endl;
     } else {
         cout << "Clave no encontrada, llamando a MOTOR DE BÚSQUEDA" << std::endl;
+    }
+
+    archivo.close();
 
     //Comienza el proceso de búsqueda de la palabra
     return 0;
---
+++ motordebusqueda.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+using namespace std;
+
+#define PORT 2021
+
+int main(){
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
+        perror("Hubo un error creando el socket");
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) != 0) {
+        perror("Hubo un error en setsockopt para SO_REUSEADDR");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEPORT, &opt, sizeof(opt)) != 0) {
+        perror("Hubo un error en setsockopt para SO_REUSEPORT");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0){
+        perror("Hubo un error en el bind");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("Hubo un error en el listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    cout << "Servidor corriendo y esperando conexiones port: " << PORT << endl;
+
+    bool seguir = true;
+
+    while (seguir){
+        if((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0){
+            perror("Hubo un error en el accept");
+            continue;
+        }
+
+        char buffer[1024] = {0};
+        int valorleido = read(new_socket, buffer, 1024);
+        string mensaje(buffer);
+
+        cout << "Mensaje: " << mensaje << endl;
+
+        if (mensaje == "SALIR AHORA"){
+            cout << "Cerrando servidor" << endl;
+            seguir = false;
+        }
+        else{
+            string respuesta = "Mensaje recibido: " + mensaje;
+            send(new_socket, respuesta.c_str(), respuesta.size(), 0);
+        }
+        close(new_socket);
+    }
+
+    close(server_fd);
+
+    return 0;
+}
